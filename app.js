e1efe59d4e127aa91409287073c504c8568a328f
nashimoff <159990431+nashimoff@users.noreply.github.com>
2025-11-28 00:12:07 +0600

    dd

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>')
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express!</h1>')
 });
